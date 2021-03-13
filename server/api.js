43c1e97cb83ad5b8899e2d817061abd81fca6245
Altom <Altomhussain2001@yahoo.com>
2021-03-13 20:24:56 +0000

    Added put enpoints to update a house

--- server/api.js
+++ server/api.js
@@ -16,29 +16,33 @@ router.get("/houses", async (req, res) => {
   const results = await Connection.query(query);
   res.json(results.rows);
 });
+
+//Post a new house
 router.post("/house", async (req, res) => {
   const {
     houseType,
     houseDescription,
     houseSold,
-    houseAddress,
+    streetName,
     housePostcode,
     housePrice,
     houseCity,
     houseImage,
+	houseNumber
   } = req.body;
 
   try {
-    const insertQuery = `INSERT INTO houses(house_type, house_description, house_sold, house_address, house_postcode, house_price, house_city, house_image) values($1, $2, $3, $4, $5, $6, $7, $8) RETURNING *`;
+    const insertQuery = `INSERT INTO houses(house_type, house_description, house_sold, street_name, house_postcode, house_price, house_city, house_image, house_number) values($1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING *`;
     const results = await Connection.query(insertQuery, [
       houseType,
       houseDescription,
       houseSold,
-      houseAddress,
+      streetName,
       housePostcode,
       housePrice,
       houseCity,
       houseImage,
+      houseNumber,
     ]);
     res.json({
       sucess: "Success",
@@ -48,5 +52,43 @@ router.post("/house", async (req, res) => {
     console.log(error.message);
   }
 });
+
+//Upate existing house
+router.put("/house/:id", async(req, res)=>{
+	const houseId = Number(req.params.id)
+	 const {
+     houseType,
+     houseDescription,
+     houseSold,
+	 streetName,
+     housePostcode,
+     housePrice,
+     houseCity,
+     houseImage,
+	 houseNumber,
+   } = req.body;
+
+   try {
+	   let updateQuery = `update houses set house_type=$1, house_description=$2, house_sold=$3, street_name=$4, house_postcode=$5, house_price=$6, house_city=$7, house_image=$8 , house_number=$9 where house_id=$10 `;
+	  await Connection.query(updateQuery, [
+       houseType,
+       houseDescription,
+       houseSold,
+       streetName,
+       housePostcode,
+       housePrice,
+       houseCity,
+       houseImage,
+       houseNumber,
+       houseId,
+     ]);
+	 res.json({
+		 success: "Updated successfully",
+	 })
+   } catch (error) {
+	   console.log(error.message);
+   }
+})
+
 
 export default router;
