77acbddddd05a0207785dc52d8bd5bd35f992f48
Altom <Altomhussain2001@yahoo.com>
2021-03-13 20:55:54 +0000

    Added delete enpoint to delete a house

--- server/api.js
+++ server/api.js
@@ -28,7 +28,7 @@ router.post("/house", async (req, res) => {
     housePrice,
     houseCity,
     houseImage,
-	houseNumber
+    houseNumber,
   } = req.body;
 
   try {
@@ -54,41 +54,60 @@ router.post("/house", async (req, res) => {
 });
 
 //Upate existing house
-router.put("/house/:id", async(req, res)=>{
-	const houseId = Number(req.params.id)
-	 const {
-     houseType,
-     houseDescription,
-     houseSold,
-	 streetName,
-     housePostcode,
-     housePrice,
-     houseCity,
-     houseImage,
-	 houseNumber,
-   } = req.body;
+router.put("/house/:id", async (req, res) => {
+  try {
+    const houseId = Number(req.params.id);
+    const {
+      houseType,
+      houseDescription,
+      houseSold,
+      streetName,
+      housePostcode,
+      housePrice,
+      houseCity,
+      houseImage,
+      houseNumber,
+    } = req.body;
 
-   try {
-	   let updateQuery = `update houses set house_type=$1, house_description=$2, house_sold=$3, street_name=$4, house_postcode=$5, house_price=$6, house_city=$7, house_image=$8 , house_number=$9 where house_id=$10 `;
-	  await Connection.query(updateQuery, [
-       houseType,
-       houseDescription,
-       houseSold,
-       streetName,
-       housePostcode,
-       housePrice,
-       houseCity,
-       houseImage,
-       houseNumber,
-       houseId,
-     ]);
-	 res.json({
-		 success: "Updated successfully",
-	 })
-   } catch (error) {
-	   console.log(error.message);
-   }
-})
+    let updateQuery = `update houses set house_type=$1, house_description=$2, house_sold=$3, street_name=$4, house_postcode=$5, house_price=$6, house_city=$7, house_image=$8 , house_number=$9 where house_id=$10 `;
+    await Connection.query(updateQuery, [
+      houseType,
+      houseDescription,
+      houseSold,
+      streetName,
+      housePostcode,
+      housePrice,
+      houseCity,
+      houseImage,
+      houseNumber,
+      houseId,
+    ]);
+    res.json({
+      success: "Updated successfully",
+    });
+  } catch (error) {
+    console.log(error.message);
+  }
+});
 
+//Delete a house
+router.delete("/house/:id", async (req, res) => {
+  try {
+    const houseId = Number(req.params.id);
 
+    const selectQuery = `select * from houses where house_id= $1`;
+    const deleteQuery = `delete from houses where house_id=$1`;
+
+    const checkHouseExist = await Connection.query(selectQuery, [houseId]);
+    await Connection.query(deleteQuery, [houseId]);
+
+    if (checkHouseExist.rows.length !== 0) {
+      res.json("deleted successfully");
+    } else {
+      res.json(`House with the id: ${houseId} does not exist sorry`);
+    }
+  } catch (error) {
+    console.log(error.message);
+  }
+});
 export default router;
