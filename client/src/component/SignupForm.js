df90fe6d53b92cc22f5fcf0d823cb5f80c7a2d43
Altom <Altomhussain2001@yahoo.com>
2021-03-18 23:05:06 +0000

    added some styling to the sign up form

--- client/src/component/SignupForm.js
+++ client/src/component/SignupForm.js
@@ -1,21 +1,22 @@
 import React from "react";
-
+import "./SignupForm.css"
 export default function SignupForm() {
   return (
-    <div>
-      <form>
+    <div className="form-container">
+      <form className="form-group">
         <label>First Name</label>
-        <input type="text" placeholder="First name" />
+        <input type="text" placeholder="First name" className="form-control"/>
         <label>Surname</label>
-        <input type="text" placeholder="Surname" />
+        <input type="text" placeholder="Surname" className="form-control"/>
         <label>Email Address</label>
-        <input type="text" placeholder="Email address" />
+        <input type="text" placeholder="Email address" className="form-control"/>
         <label>Confirm Email address</label>
-        <input type="text" placeholder="Confirm email address" />
+        <input type="text" placeholder="Confirm email address" className="form-control"/>
         <label>City</label>
-        <input type="text" placeholder="City" />
+        <input type="text" placeholder="City" className="form-control"/>
         <label>Phone number</label>
-        <input type="text" placeholder="" />
+        <input type="text" placeholder="" className="form-control"/>
+        <button className="btn btn-success form-control">Submit</button>
       </form>
     </div>
   );
