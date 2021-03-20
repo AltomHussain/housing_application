45ef663f248c7d27425fc57e5cff9ec2a416bdb5
Altom <Altomhussain2001@yahoo.com>
2021-03-20 22:56:46 +0000

    added error message to all the fields

--- client/src/component/SignupForm.js
+++ client/src/component/SignupForm.js
@@ -2,85 +2,26 @@ import React, { useState, useEffect } from "react";
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import "./SignupForm.css";
-import { sendErrors } from "./Errors";
+
 import content from "./statics/InputsData";
 import * as yup from "yup";
 
 export default function SignupForm() {
-
   console.log(useForm());
   let schema = yup.object().shape({
     firstName: yup.string().required("s;lakfdj"),
     surname: yup.string().required(),
-     email: yup.string().required(),
-     password: yup.string().required().min(5),
-  confirmEmail: yup.string().required(),
-    // city: yup.string().required(),
-    // phoneNumber: yup.number().required().positive().integer(),
+    email: yup.string().required(),
+    password: yup.string().required().min(5),
+    confirmEmail: yup.string().required(),
+    city: yup.string().required(),
+    phoneNumber: yup.number().required().positive(),
   });
   const { register, handleSubmit, errors } = useForm({
     resolver: yupResolver(schema),
   });
 
-  // const [input, setInput] = useState({
-  //   firstName: "",
-  //   surname: "",
-  //   email: "",
-  //   confirmEmail: "",
-  //   password: "",
-  //   city: "",
-  //   phoneNumber: "",
-  // });
-
-  // const [firstNameError, setFirstNameError] = useState("");
-  // const [surnameError, setSurnameError] = useState("");
-  // const [emailError, setEmailError] = useState("");
-  // const [confirmError, setConfirmError] = useState("");
-  // const [passwordError, setPasswordError] = useState("");
-  // const [cityError, setcityError] = useState("");
-  // const [phoneError, setPhoneError] = useState("");
-  // const [isValid, setIsValid] = useState(false);
-  // const {
-  //   firstName,
-  //   surname,
-  //   email,
-  //   confirmEmail,
-  //   city,
-  //   phoneNumber,
-  //   password,
-  // } = input;
-
-  // console.log(isValid === true);
-  // const handleChange = (e) => {
-  //   const updateValues = {
-  //     ...input,
-  //     [e.target.name]: e.target.value,
-  //   };
-  //   setInput(updateValues);
-  // };
-  // const handleSubmit = (e) => {
-  //   e.preventDefault();
-  //   sendErrors(
-  //     firstName,
-  //     surname,
-  //     email,
-  //     confirmEmail,
-  //     password,
-  //     city,
-  //     phoneNumber,
-  //     setFirstNameError,
-  //     setSurnameError,
-  //     setEmailError,
-  //     setConfirmError,
-  //     setcityError,
-  //     setPhoneError,
-  //     setPasswordError,
-  //     setInput,
-  //     setIsValid
-  //   );
-
-  //   test();
-  // };
+  
   // console.log(content.inputs);
   // const test = () => {
   //   fetch("/api/register", {
@@ -102,11 +43,12 @@ export default function SignupForm() {
   //     .catch((error) => console.log(error));
   // };
   const onSubmit = (data) => console.log(data);
- console.log(errors);
+  console.log(errors);
   return (
     <div className="form-container">
       <form className="form-group" onSubmit={handleSubmit(onSubmit)}>
         {content.inputs.map((input, index) => {
+          console.log(input.name);
           return (
             <div key={index}>
               <label>{input.label}</label>
@@ -117,81 +59,10 @@ export default function SignupForm() {
                 className="form-control"
                 ref={register}
               />
-           
+              <p>{errors[input.name]?.message}</p>
             </div>
           );
         })}
-
-        {/* <label>First Name</label>
-        <input
-          type="text"
-          placeholder="First name"
-          className="form-control"
-          value={firstName}
-          onChange={handleChange}
-          name="firstName"
-        />
-        {firstNameError && <p>{firstNameError}*</p>}
-        <label>Surname</label>
-        <input
-          type="text"
-          placeholder="Surname"
-          className="form-control"
-          value={surname}
-          onChange={handleChange}
-          name="surname"
-        />
-        {surnameError && <p>{surnameError}*</p>}
-        <label>Email Address</label>
-        <input
-          type="text"
-          placeholder="Email address"
-          className="form-control"
-          value={email}
-          onChange={handleChange}
-          name="email"
-        />
-        {emailError && <p>{emailError}*</p>}
-        <label>Confirm Email address</label>
-        <input
-          type="text"
-          placeholder="Confirm email address"
-          className="form-control"
-          value={confirmEmail}
-          onChange={handleChange}
-          name="confirmEmail"
-        />
-        {confirmError && <p>{confirmError}*</p>}
-        <label>City</label>
-        <input
-          type="text"
-          placeholder="City"
-          className="form-control"
-          value={city}
-          onChange={handleChange}
-          name="city"
-        />
-        {cityError && <p>{cityError}*</p>}
-        <label>Password</label>
-        <input
-          type="text"
-          placeholder="Password"
-          className="form-control"
-          value={password}
-          onChange={handleChange}
-          name="password"
-        />
-        {passwordError && <p>{passwordError}*</p>}
-        <label>Phone number</label>
-        <input
-          type="text"
-          placeholder=""
-          className="form-control"
-          value={phoneNumber}
-          onChange={handleChange}
-          name="phoneNumber"
-        />
-        {phoneError && <p>{phoneError}*</p>} */}
         <button type="submit" className="btn btn-success form-control">
           Submit
         </button>
