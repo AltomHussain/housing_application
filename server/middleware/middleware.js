1eb1f50faaa64c831dd1e1e304c42a6674776247
altom <altomhussian2001@yahoo.com>
2021-03-22 22:06:18 +0000

    just added logs for testing

--- server/middleware/middleware.js
+++ server/middleware/middleware.js
@@ -16,6 +16,7 @@ export const logErrors = () => (err, _, res, next) => {
 };
 
 export const pushStateRouting = (apiRoot, staticDir) => (req, res, next) => {
+
 	if (req.method === "GET" && !req.url.startsWith(apiRoot)) {
 		return res.sendFile(path.join(staticDir, "index.html"));
 	}
