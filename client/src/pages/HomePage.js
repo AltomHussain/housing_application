56dc3110f5eae7db82a41bfe1d7edf0a40506db3
altom <altomhussian2001@yahoo.com>
2021-03-26 21:30:48 +0000

    added some styling to the home page

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -8,44 +8,46 @@ export default function HomePage() {
   console.log(allHouses);
   return (
     <div>
-      <Header home="Home" />
-      {allHouses.map(
-        ({
-          house_id,
-          house_type,
-          house_description,
-          house_sold,
-          street_name,
-          house_postcode,
-          house_price,
-          house_city,
-          house_image,
-          house_number,
-        }) => {
-          return (
-            <article className="room">
-              <div className="img-container">
-                <img
-                  src={
-                    house_image
-                      ? house_image
-                      : "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/primrose-hill-house-1576670230.png?crop=1xw:1xh;center,top&resize=480:*"
-                  }
-                  alt="single room"
-                />
-                <div className="price-top">
-                  <h6>{house_price}</h6>
-                  <p>per night</p>
+        <Header home="Home" />
+      <div className="all-houses">
+        {allHouses.map(
+          ({
+            house_id,
+            house_type,
+            house_description,
+            house_sold,
+            street_name,
+            house_postcode,
+            house_price,
+            house_city,
+            house_image,
+            house_number,
+          }) => {
+            return (
+              <article className="room">
+                <div className="img-container">
+                  <img
+                    src={
+                      house_image
+                        ? house_image
+                        : "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/primrose-hill-house-1576670230.png?crop=1xw:1xh;center,top&resize=480:*"
+                    }
+                    alt="single-room"
+                  />
+                  <div className="price-top">
+                    <h6>{house_price}</h6>
+                    <p>per night</p>
+                  </div>
+                  <Link to="/signup" className="btn-primary room-link">
+                    features
+                  </Link>
                 </div>
-                <Link to="/signup" className="btn-primary room-link">
-                  features
-                </Link>
-              </div>
-              <p className="room-info">Hello</p>
-            </article>
-          );
-        }
-      )}
+                <p className="room-info">Hello</p>
+              </article>
+            );
+          }
+        )}
+      </div>
     </div>
   );
 }
