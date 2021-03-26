78e9ea762725a3fe525c00accd0339ba4e94b26b
altom <altomhussian2001@yahoo.com>
2021-03-26 21:21:13 +0000

    displayed all fetched houses

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,29 +1,51 @@
-import React, {useContext} from 'react'
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
-import Header from '../component/Header';
-import "./HomePage.css"
+import Header from "../component/Header";
+import { GetAllHouses } from "../component/Context/GetAllHouses";
+import "./HomePage.css";
 export default function HomePage() {
-
-    return (
-      <div>
-        <Header home="Home"/>
-        <article className="room">
-          <div className="img-container">
-            <img
-              src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg"
-              alt="single room"
-            />
-            <div className="price-top">
-              <h6>444</h6>
-              <p>per night</p>
-            </div>
-            <Link to="/signup" className="btn-primary room-link">
-              features
-            </Link>
-          </div>
-          <p className="room-info">Hello</p>
-        </article>
-      </div>
-    );
-    
+  const { allHouses } = useContext(GetAllHouses);
+  console.log(allHouses);
+  return (
+    <div>
+      <Header home="Home" />
+      {allHouses.map(
+        ({
+          house_id,
+          house_type,
+          house_description,
+          house_sold,
+          street_name,
+          house_postcode,
+          house_price,
+          house_city,
+          house_image,
+          house_number,
+        }) => {
+          return (
+            <article className="room">
+              <div className="img-container">
+                <img
+                  src={
+                    house_image
+                      ? house_image
+                      : "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/primrose-hill-house-1576670230.png?crop=1xw:1xh;center,top&resize=480:*"
+                  }
+                  alt="single room"
+                />
+                <div className="price-top">
+                  <h6>{house_price}</h6>
+                  <p>per night</p>
+                </div>
+                <Link to="/signup" className="btn-primary room-link">
+                  features
+                </Link>
+              </div>
+              <p className="room-info">Hello</p>
+            </article>
+          );
+        }
+      )}
+    </div>
+  );
 }
