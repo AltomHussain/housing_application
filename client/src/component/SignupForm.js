1426d28c19242438825f7d62721c6daed3fc806e
altom <altomhussian2001@yahoo.com>
2021-03-27 12:10:50 +0000

    added styling to the signup form

--- client/src/component/SignupForm.js
+++ client/src/component/SignupForm.js
@@ -33,7 +33,7 @@ export default function SignupForm() {
       .then((res) => {
         if (res.ok) {
           reset();
-          history.push("/home")
+          history.push("/home");
         }
         return res.json();
       })
@@ -43,34 +43,34 @@ export default function SignupForm() {
 
   const onSubmit = (data) => {
     sendData(data);
-    
   };
-    let back=  <i className="fas fa-long-arrow-alt-left me-2">Back</i>;
+  let back = <i className="fas fa-long-arrow-alt-left me-2">Back</i>;
   return (
-    <>
-    <Header back={back}/>
-    <div className="form-container">
-      <form className="form-group" onSubmit={handleSubmit(onSubmit)}>
-        <h5 className="text-center">{!dbData.success ? dbData : null}</h5>
-        {content.inputs.map(({ type, id, label, name }) => {
-          return (
-            <div key={id}>
-              <label>{label}</label>
-              <input
-                type={type}
-                placeholder={label}
-                name={name}
-                className="form-control"
-                ref={register}
-              />
-              <p className="text-center">{errors[name]?.message}</p>
-            </div>
-          );
-        })}
-        <button type="submit" className="btn btn-success form-control">
-          Submit
-        </button>
-      </form>
+    <div className="container-big">
+      <Header back={back} />
+      <div className="form-container">
+        <form className="form-group" onSubmit={handleSubmit(onSubmit)}>
+          <h5 className="text-center">{!dbData.success ? dbData : null}</h5>
+          {content.inputs.map(({ type, id, label, name }) => {
+            return (
+              <div key={id}>
+                <label className="signup-label">{label}</label>
+                <input
+                  type={type}
+                  placeholder={label}
+                  name={name}
+                  className="form-control"
+                  ref={register}
+                />
+                <p className="text-center">{errors[name]?.message}</p>
+              </div>
+            );
+          })}
+          <button type="submit" className="btn btn-success form-control">
+            Submit
+          </button>
+        </form>
+      </div>
     </div>
- </> );
+  );
 }
