e16062a206eff6c7724dacd9ae268fed152ec7cd
altom <altomhussian2001@yahoo.com>
2021-03-28 13:00:45 +0100

    fetched data of one house successfully

--- client/src/component/GetOneHouse.js
+++ client/src/component/GetOneHouse.js
@@ -1,19 +1,42 @@
-import React, {useEstate, useEffect, useState} from 'react'
-
-export default function GetOneHouse({id}) {
+import React, {useEffect, useState} from 'react'
+import { useHistory, useParams } from "react-router-dom";
+export default function GetOneHouse() {
     const [oneHouse, setOneHouse] = useState([])
+    const {id} = useParams()
     const GetOneHouseDetail = ()=>{
         fetch(`/api/house/${id}`)
         .then((res)=> res.json())
         .then((data)=>setOneHouse(data))
         .catch((error)=>console.log(error))
     }
-useEffect(()=>{
+    useEffect(()=>{
     GetOneHouseDetail()
 }, [])
+
     return (
-        <div>
-            helloooeo
-        </div>
-    )
+      <div>
+        {oneHouse.map(
+          ({
+            house_id,
+            house_type,
+            house_description,
+            house_sold,
+            street_name,
+            house_postcode,
+            house_price,
+            house_city,
+            house_image,
+            house_number,
+          }) => {
+            return (
+              <div>
+                <div className="house-image">
+                  <img src={house_image} alt="house-image" />{" "}
+                </div>
+              </div>
+            );
+          }
+        )}
+      </div>
+    );
 }
