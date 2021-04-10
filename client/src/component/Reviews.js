4d4979f8f66522d1962729ef17c2068099a1b4e1
altom <altomhussian2001@yahoo.com>
2021-04-10 20:52:41 +0100

    feched the house reviews

--- client/src/component/Reviews.js
+++ client/src/component/Reviews.js
@@ -1,29 +1,60 @@
-import React, { useState, useEffect } from 'react'
-import {useParams} from "react-router-dom"
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import {Card} from "react-bootstrap"
 export default function Reviews() {
-    const [allReviews, setAllReviews] = useState([])
-    const {id} = useParams();
+  const [allReviews, setAllReviews] = useState([]);
+  const { id } = useParams();
 
- const getReview = () =>{
-       fetch(`/api/house/${id}`)
-         .then((res) => res.json())
-         .then((data) =>
-           setAllReviews({
-             review: data.data.reviews,
-           })
-         )
-         .catch((error) => console.log(error));
- }
-useEffect(() => {
-   getReview()
-}, [id])
-console.log(allReviews? allReviews.review: "no");
+  const getReview = () => {
+    fetch(`/api/house/${id}`)
+      .then((res) => res.json())
+      .then((data) =>
+        setAllReviews({
+          review: data.data.reviews,
+        })
+      )
+      .catch((error) => console.log(error));
+  };
+  useEffect(() => {
+    getReview();
+  }, [id]);
+  console.log(allReviews ? allReviews.review : "no");
 
-
-
-    return (
-        <div>
-            heloo reviews
-        </div>
-    )
+  return (
+    <div>
+      {allReviews.review
+        ? allReviews.review.map(
+            ({
+              id,
+              date_added,
+              bid,
+              reviewer_name,
+              review_description,
+              rating,
+            }) => {
+              return (
+                <div>
+                  <Card
+                    bg="success"
+                    key={id}
+                    text="light"
+                    style={{ width: "18rem" }}
+                    className="mb-2"
+                  >
+                    <Card.Header>Header</Card.Header>
+                    <Card.Body>
+                      <Card.Title> Card Title </Card.Title>
+                      <Card.Text>
+                        Some quick example text to build on the card title and
+                        make up the bulk of the card's content.
+                      </Card.Text>
+                    </Card.Body>
+                  </Card>
+                </div>
+              );
+            }
+          )
+        : "not data yet"}
+    </div>
+  );
 }
