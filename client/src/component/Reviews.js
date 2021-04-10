6747834d393a5f87977e21a337e048d79d927501
altom <altomhussian2001@yahoo.com>
2021-04-10 21:23:51 +0100

    styled the review cards

--- client/src/component/Reviews.js
+++ client/src/component/Reviews.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import "./Reviews.css"
-import {Card} from "react-bootstrap"
+import {Card, Spinner, animation} from "react-bootstrap"
 export default function Reviews() {
   const [allReviews, setAllReviews] = useState([]);
   const { id } = useParams();
@@ -22,40 +22,45 @@ export default function Reviews() {
 
   return (
     <div className="reviews-container">
-      {allReviews.review
-        ? allReviews.review.map(
-            ({
-              id,
-              date_added,
-              bid,
-              reviewer_name,
-              review_description,
-              rating,
-            }) => {
-              console.log(rating);
-              return (
-                <div key={id} className="reviews">
-                  <Card
-                    bg="success"
-                    text="light"
-                    style={{ width: "18rem" }}
-                    className="mb-2"
-                  >
-                    <Card.Header>
-                      <h4>
-                        {reviewer_name} rating:{rating}
-                      </h4>{" "}
-                    </Card.Header>
-                    <Card.Body>
-                      <Card.Title> {date_added} </Card.Title>
-                      <Card.Text>{review_description}.</Card.Text>
-                    </Card.Body>
-                  </Card>
-                </div>
-              );
-            }
-          )
-        : "not data yet"}
+      {allReviews.review ?(
+        allReviews.review.map(
+          ({
+            id,
+            date_added,
+            bid,
+            reviewer_name,
+            review_description,
+            rating,
+          }) => {
+            console.log(rating);
+            return (
+              <div key={id} className="reviews">
+                <Card
+                  bg="success"
+                  text="light"
+                  style={{ width: "18rem" }}
+                  className="mb-2"
+                >
+                  <Card.Header>
+                    <h4>
+                      {reviewer_name} rating:{rating}
+                    </h4>{" "}
+                  </Card.Header>
+                  <Card.Body>
+                    <Card.Title> {date_added} </Card.Title>
+                    <Card.Text>{review_description}.</Card.Text>
+                  </Card.Body>
+                </Card>
+              </div>
+            );
+          }
+        )
+      ) : (
+          <>
+      loading... 
+       <Spinner animation="grow" />
+       </>
+      )}
     </div>
   );
 }
