28d28a34412c0898763e4c3ca32f6fd708800aa9
altom <altomhussian2001@yahoo.com>
2021-04-11 13:50:40 +0100

    added useForm validation

--- client/src/component/AddReview.js
+++ client/src/component/AddReview.js
@@ -1,33 +1,37 @@
-import React from 'react'
-import "./AddReview.css"
+import React from "react";
+import "./AddReview.css";
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
 export default function AddReview() {
-    const scheme = yup.object().shape({
-name: yup.string().required("Field is required"),
-rating: yup.string().required("Fied is required"),
-date: yup.string().required("Please select a date"),
-description:  yup.string().required("Please add a review")
+  const scheme = yup.object().shape({
+    name: yup.string().required("Field is required"),
+    rating: yup.string().required("Fied is required"),
+    date: yup.string().required("Please select a date"),
+    description: yup.string().required("Please add a review"),
+  });
 
-    })
+  const { register, handleSubmit, errors, reset } = useForm({
+    resolver: yupResolver(scheme),
+  });
 
-const {
-  register,
-  handleSubmit,
-errors
-  
-} = useForm({});
-
-
-    return (
-      <div className="add-review-container">
-          <h3 className="text-center">Add Review</h3>
-          <form>
+  const onSubmit = (data) =>{
+      console.log(data);
+  }
+  return (
+    <div className="add-review-container">
+      <h3 className="text-center">Add Review</h3>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <label>Name</label>
-        <input type="text" placeholder="Name" className="form-control" name="name"/>
+        <input
+          type="text"
+          placeholder="Name"
+          className="form-control"
+          name="name"
+          ref={register}
+        />
         <label>Rating</label>
-        <select className="form-control" name="rating">
+        <select className="form-control" name="rating" ref={register}>
           <option value="1">1</option>
           <option value="2">2</option>
           <option value="3">3</option>
@@ -35,12 +39,23 @@ errors
           <option value="5">5</option>
         </select>
         <label>Date</label>
-        <input type="date" placeholder="Name" className="form-control" name="date" />
+        <input
+          type="date"
+          placeholder="Name"
+          className="form-control"
+          name="date"
+          ref={register}
+        />
         <label>Review</label>
-        <textarea className="form-control" placeholder="Add Review" name="description"></textarea>
-    
-    <button className="btn btn-success form-control mt-2">Add</button>
-    </form>
-      </div>
-    );
+        <textarea
+          className="form-control"
+          placeholder="Add Review"
+          name="description"
+          ref={register}
+        ></textarea>
+
+        <button className="btn btn-success form-control mt-2">Add</button>
+      </form>
+    </div>
+  );
 }
