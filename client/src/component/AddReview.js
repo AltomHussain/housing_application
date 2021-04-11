de06298f1f5fc74455475fea38b9c3ba8afe52c5
altom <altomhussian2001@yahoo.com>
2021-04-11 13:38:23 +0100

    added added component and its form

--- client/src/component/AddReview.js
+++ client/src/component/AddReview.js
@@ -1,9 +1,27 @@
 import React from 'react'
-
+import "./AddReview.css"
 export default function AddReview() {
     return (
-        <div>
-        
-        </div>
-    )
+      <div className="add-review-container">
+          <h3 className="text-center">Add Review</h3>
+          <form>
+        <label>Name</label>
+        <input type="text" placeholder="Name" className="form-control" />
+        <label>Rating</label>
+        <select className="form-control">
+          <option value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+          <option value="4">4</option>
+          <option value="5">5</option>
+        </select>
+        <label>Date</label>
+        <input type="date" placeholder="Name" className="form-control" />
+        <label>Review</label>
+        <textarea className="form-control" placeholder="Add Review" ></textarea>
+    
+    <button className="btn btn-success form-control mt-2">Add</button>
+    </form>
+      </div>
+    );
 }
--- client/src/component/Reviews.js
+++ client/src/component/Reviews.js
@@ -3,6 +3,7 @@ import { useParams } from "react-router-dom";
 import "./Reviews.css"
 import {Card, Spinner, animation} from "react-bootstrap";
 import StarRating from "./StarRating"
+import AddReview from "./AddReview";
 export default function Reviews() {
   const [allReviews, setAllReviews] = useState([]);
   const { id } = useParams();
@@ -22,45 +23,50 @@ export default function Reviews() {
   }, [id]);
 
   return (
+     <div className="review-add-review-container">
     <div className="reviews-container">
-      {allReviews.review ?(
-        allReviews.review.map(
-          ({
-            id,
-            date_added,
-            bid,
-            reviewer_name,
-            review_description,
-            rating,
-        
-          }) => {
-        
-            return (
-              <div key={id} className="reviews">
-                <Card
-                  bg="success"
-                  text="light"
-                  style={{ width: "18rem" }}
-                  className="mb-2"
-                >
-                  <Card.Header className="d-flex justify-content-between">
-                    <h4>{reviewer_name} </h4> <p><StarRating rating={rating} /></p>
-                  </Card.Header>
-                  <Card.Body>
-                    <Card.Title> {date_added} </Card.Title>
-                    <Card.Text>{review_description}.</Card.Text>
-                  </Card.Body>
-                </Card>
-              </div>
-            );
-          }
-        )
-      ) : (
+     
+        {allReviews.review ? (
+          allReviews.review.map(
+            ({
+              id,
+              date_added,
+              bid,
+              reviewer_name,
+              review_description,
+              rating,
+            }) => {
+              return (
+                <div key={id} className="reviews">
+                  <Card
+                    bg="success"
+                    text="light"
+                    style={{ width: "18rem" }}
+                    className="mb-2"
+                  >
+                    <Card.Header className="d-flex justify-content-between">
+                      <h4>{reviewer_name} </h4>{" "}
+                      <p>
+                        <StarRating rating={rating} />
+                      </p>
+                    </Card.Header>
+                    <Card.Body>
+                      <Card.Title> {date_added} </Card.Title>
+                      <Card.Text>{review_description}.</Card.Text>
+                    </Card.Body>
+                  </Card>
+                </div>
+              );
+            }
+          )
+        ) : (
           <>
-        loading... 
-       <Spinner animation="grow" />
-       </>
-      )}
+            loading...
+            <Spinner animation="grow" />
+          </>
+        )}
+      </div>
+        <AddReview />
     </div>
   );
 }
