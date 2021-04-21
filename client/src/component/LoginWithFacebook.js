0836eb36e79084d2dc9fb0212be42838f7df8cca
altom <altomhussian2001@yahoo.com>
2021-04-21 23:17:01 +0100

    tried to add facebook login auth but still not working

--- client/src/component/LoginWithFacebook.js
+++ client/src/component/LoginWithFacebook.js
@@ -2,18 +2,28 @@ import React from 'react'
 import FacebookLogin from "react-facebook-login";
 export default function LoginWithFacebook() {
     const responseFacebook = (response) => {
-      console.log(response);
+    //   console.log((response?response.id: ""));
+    //   fetch("/api/facebook-login", {
+    //       method: "POST",
+    //       headers: {"Content-Type": "application/json"},
+    //       body: JSON.stringify({
+    //           id: response
+    //       })
+    //   })
+    //   .then(res=> res.json())
+    //   .then(data=>console.log(data))
     };
     return (
       <div>
         <FacebookLogin
           appId="766695197364835"
+        //   client_secrect="703053617a2ab44d2be9e4abd9ce9091"
           autoLoad={true}
           fields="name,email,picture"
-          onClick={responseFacebook}
+          onClick={()=>responseFacebook()}
           callback={responseFacebook}
         />
-        ,
+        
       </div>
     );
 }
