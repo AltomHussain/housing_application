e6b772617566f1794fefd4954fa6b5dfe01edc47
altom <altomhussian2001@yahoo.com>
2021-04-22 18:14:17 +0100

    added facebook login successfully

--- client/src/component/LoginWithFacebook.js
+++ client/src/component/LoginWithFacebook.js
@@ -1,17 +1,26 @@
 import React from 'react'
 import FacebookLogin from "react-facebook-login";
+import {useHistory} from "react-router-dom"
 export default function LoginWithFacebook() {
+    const history = useHistory()
+    const handleData =(data)=>{
+        console.log(data);
+    }
     const responseFacebook = (response) => {
-    //   console.log((response?response.id: ""));
-    //   fetch("/api/facebook-login", {
-    //       method: "POST",
-    //       headers: {"Content-Type": "application/json"},
-    //       body: JSON.stringify({
-    //           id: response
-    //       })
-    //   })
-    //   .then(res=> res.json())
-    //   .then(data=>console.log(data))
+      fetch("/api/facebook-login", {
+          method: "POST",
+          headers: {"Content-Type": "application/json"},
+          body: JSON.stringify({
+              id: response.id,
+              name: response.name,
+              email: response.email
+          })
+      })
+      .then(res=> res.json())
+      .then(data=>console.log(data))
+      if (response.id) {
+        history.push("/home");
+      }
     };
     return (
       <div>
@@ -20,7 +29,7 @@ export default function LoginWithFacebook() {
         //   client_secrect="703053617a2ab44d2be9e4abd9ce9091"
           autoLoad={true}
           fields="name,email,picture"
-          onClick={()=>responseFacebook()}
+          onClick={handleData}
           callback={responseFacebook}
         />
         
