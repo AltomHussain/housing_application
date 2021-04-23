ce35498ee2a0629552ecab1f4a41f70fd71ae2b2
altom <altomhussian2001@yahoo.com>
2021-04-23 19:30:37 +0100

    added filter houses successfully

--- client/src/component/SelectDropdown.js
+++ client/src/component/SelectDropdown.js
@@ -1,14 +1,14 @@
 import React, { useState } from 'react'
 
-export default function SelectDropdown() {
-const [select, setSeletc ] = useState("")
-    return (
-        <div>
-            <select >
-                <option value="select all">Select All</option>
-                <option value="rent">Rent</option>
-                <option value="for sale">For Sale</option>
-            </select>
-        </div>
-    )
+export default function SelectDropdown({ setSelect, filterHouse, allHouses }) {
+  return (
+    <div>
+      <select onChange={(e) => setSelect(e.target.value)}>
+        <option value="Select All">Select All</option>
+        <option value="rent">Rent</option>
+        <option value="for sale">For Sale</option>
+      </select>
+      <p>{`${filterHouse.length}/${allHouses.length} houses`}</p>
+    </div>
+  );
 }
