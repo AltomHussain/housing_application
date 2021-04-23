ecaadd69f55e4e83a403d5c259aa3575307986d5
altom <altomhussian2001@yahoo.com>
2021-04-23 10:16:37 +0100

    added select component

---
+++ client/src/component/SelectDropdown.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function SelectDropdown() {
+    
+    return (
+        <div>
+            <select >
+                <option value=""></option>
+                <option value=""></option>
+                <option value=""></option>
+            </select>
+        </div>
+    )
+}
